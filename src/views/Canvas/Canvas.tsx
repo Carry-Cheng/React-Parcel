608e41a0cd8f18e7a695e9c816776cb135e160ac
zhoucheng <1057415350@qq.com>
2019-05-17 18:27:49 +0800

    test

--- src/views/Canvas/Canvas.tsx
+++ src/views/Canvas/Canvas.tsx
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
 
-export default class Canvas extends Component {
+import React, { Component } from 'react';
+const Style = require('./canvas.less');
+
+interface CanvasProps {
+	
+}
+ 
+interface CanvasState {
+	canvasWidth: number,
+	canvasHeigth: number
+}
+
+export default class Canvas extends Component<CanvasProps, CanvasState> {
+	constructor(props: CanvasProps) {
+		super(props);
+		this.state = {
+			canvasWidth: document.body.clientWidth,
+			canvasHeigth: document.body.clientHeight
+		}
+	}
+
 	render() {
 		return (
-			<div>
-				Canvas. hello.
+			<div className={Style['canvas']}>
+				<canvas id="canvas" width={this.state.canvasWidth} height={this.state.canvasHeigth}></canvas>
 			</div>
 		)
+	}
+
+	componentDidMount() {
+		console.info('con')
 	}
 }
